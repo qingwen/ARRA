eaafbd119b404f798969a6cd2fd00354408f1af0
Qingwen <chelven@gmail.com>
2010-06-06 01:34:42 +0200

    correct a type error in instr 4

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@ function execute($bininstr) {
             $_SESSION['reg_s'] = (int)($tmp - $_SESSION['reg_a'] * pow(2,29));
             $_SESSION['reg_a'] = int32to30($_SESSION['reg_a']);
             $_SESSION['reg_s'] = int32to30($_SESSION['reg_s']);
-            $_SESSION['log'] = $_SESSION['instr_no'].' | '.$_SESSION['instr_ptr']."    $instr/$addr: P = [A] X Memory[$addr] + [S]; the value of [A] and [S] are specified with P = 2^29 X [A] + [S];\n".$_SESSION['log'];
+            $_SESSION['log'] = $_SESSION['instr_no'].' | '.$_SESSION['instr_ptr']."    $instr/$addr: P = [S] X Memory[$addr] + [A]; the value of [A] and [S] are specified with P = 2^29 X [A] + [S];\n".$_SESSION['log'];
             $_SESSION['instr_ptr'] = $_SESSION['instr_ptr'] + 1;
             $_SESSION['changes']['reg_a'] = 1;
             $_SESSION['changes']['reg_s'] = 1;
